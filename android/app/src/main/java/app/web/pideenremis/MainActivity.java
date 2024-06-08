dad47cc0dc3ec0a32ff87c70ecf29c29f20b7283
Juan Pablo Hurtado <juanpablo.jpho@gmail.com>
2024-06-08 18:35:35 -0500

    test android e ios

---
+++ android/app/src/main/java/app/web/pideenremis/MainActivity.java
@@ -0,0 +1,5 @@
+package app.web.pideenremis;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'remis',
+  appId: 'app.web.pideenremis',
+  appName: 'Remis',
   webDir: 'www'
 };
 
